030e00ad3316c2aa03e0b1722e2cf0bb876a0f7c
adenosinetp10 <adenosinetp10@gmail.com>
2022-12-21 21:34:39 +0530

    added LatestAnime wrapper Class

--- GDrivePlayer/anime.py
+++ GDrivePlayer/anime.py
@@ -31,6 +31,31 @@ class Anime:
         self.player_url = player_url 
 
 
+class LatestAnime:
+
+    def __init__(
+                    self, 
+                    id, 
+                    title,
+                    poster,
+                    genre,
+                    status,
+                    type,
+                    total_episode,
+                    sub,
+                    player_url
+                ) -> None:
+
+        self.id = id
+        self.title = title
+        self.poster = poster 
+        self.genre = genre
+        self.status = status
+        self.type = type
+        self.total_episode = total_episode
+        self.sub = sub
+        self.player_url = player_url 
+
 
 class GAnime(GDrivePlayer):
 
@@ -43,26 +68,55 @@ class GAnime(GDrivePlayer):
         if limit > 100:
             raise SearchLimitError(100, f"Limit cannot exceed 100 animes. But you requested {limit} animes.")
         url = f"{self.__url_anime}search?title={plus_encode(title)}&limit={limit}&page={page}"
-        print(url)
         res = jsonify(super().request(url))
         animeList = []
         for a in res:
-            animeList.append(self.__json_to_animeObj(a))
+            animeList.append(self.__json_to_animeObj(a, 1))
 
         return animeList
 
 
-    def __json_to_animeObj(self, an: dict):
+    def latestAnimes(self, limit: Optional[Union[str, int]] = 10, page: Optional[Union[str, int]] = 1, order: Optional[str] = "last_updated", sort: Optional[str] = "DESC") -> List[Anime]:
+        if limit > 100:
+            raise SearchLimitError(100, f"Limit cannot exceed 100 animes. But you requested {limit} animes.")
+        url = f"{self.__url_anime}newest?limit={limit}&page={page}&order={order}&sort={sort}"
+        res = jsonify(super().request(url))
+        animeList = []
+        for a in res:
+            animeList.append(self.__json_to_animeObj(a, 0))
 
-        return Anime(
-                id=an["id"],
-                title=an["title"],
-                poster=an["poster"],
-                genre=an["genre"],
-                summary=an["summary"],
-                status=an["status"],
-                type=an["type"],
-                total_episode=an["total_episode"],
-                sub=an["sub"],
-                player_url=gen_ep_from_player_url(an["player_url"], an["total_episode"])
-            )
+        return animeList
+
+    def __json_to_animeObj(self, an: dict, obj: bool):
+
+        if obj is True:
+
+            return Anime(
+                    id=an["id"],
+                    title=an["title"],
+                    poster=an["poster"],
+                    genre=an["genre"],
+                    summary=an["summary"],
+                    status=an["status"],
+                    type=an["type"],
+                    total_episode=an["total_episode"],
+                    sub=an["sub"],
+                    player_url=gen_ep_from_player_url(an["player_url"], an["total_episode"])
+                )
+        else:
+
+            return LatestAnime(
+                    id=an["id"],
+                    title=an["title"],
+                    poster=an["poster"],
+                    genre=an["genre"],
+                    status=an["status"],
+                    type=an["type"],
+                    total_episode=an["total_episode"],
+                    sub=an["sub"],
+                    player_url=gen_ep_from_player_url(an["player_url"], an["total_episode"])
+                )
+
+a = GAnime().latestAnimes()
+for i in a:
+    print(i.title)
